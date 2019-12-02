6e3226ffc00e9254152e8a2877221d0f17c79c50
Rizky Benggolo <travarilo@gmail.com>
2019-12-02 18:56:22 +0700

    X00T: Add libsketchology_native *Fix markup fc
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: I3a739267f5f58f37982fb23ded788c759aaa8678
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- X00T/X00T-vendor.mk
+++ X00T/X00T-vendor.mk
@@ -106,6 +106,7 @@ PRODUCT_COPY_FILES += \
     vendor/asus/X00T/proprietary/lib64/librcc.so:system/lib64/librcc.so \
     vendor/asus/X00T/proprietary/lib64/libsdm-disp-apis.so:system/lib64/libsdm-disp-apis.so \
     vendor/asus/X00T/proprietary/lib64/libsdsprpc_system.so:system/lib64/libsdsprpc_system.so \
+    vendor/asus/X00T/proprietary/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so \
     vendor/asus/X00T/proprietary/lib64/libwfdclient.so:system/lib64/libwfdclient.so \
     vendor/asus/X00T/proprietary/lib64/libwfdnative.so:system/lib64/libwfdnative.so \
     vendor/asus/X00T/proprietary/lib64/vendor.display.color@1.0.so:system/lib64/vendor.display.color@1.0.so \
